f05ee0b5a67038b10d96f4647ca06ffc978c102f
morisusanto <morisusanto@gmail.com>
2018-10-08 14:44:51 +0800

    perubahan di file latihan1.py

---
+++ latihan1.py
@@ -0,0 +1,16 @@
+a = 4
+a = 5
+
+kata = "contoh kata"
+kalimat = "contoh kalimat"
+paragraf = """kalimat berparagraf 1
+kalimat berfaragraf 2
+kalimat berfaragraf 3
+kalimat berfaragraf 4"""
+
+var_1 = True
+var_2 = False
+
+var_tuple = ("laki", "perempuan")
+var_list = ("Senin", "Selasa", "Rabu", "Kamis","Jumat","Sabtu","Ahad")
+var_dict = ()
